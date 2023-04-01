a38dee6fcf1beb818455af3ae67f2d7f2de16c5c
SCRACX <robertlupas09@gmail.com>
2023-04-01 10:30:53 +0300

    Update scroll-to-top.js

--- src/scroll-to-top.js
+++ src/scroll-to-top.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  loadCSS();
   var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
   window.addEventListener("scroll", function () {
@@ -19,3 +20,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+function loadCSS() {
+  var head = document.getElementsByTagName("HEAD")[0];
+
+  // Create new link Element
+  var link = document.createElement("link");
+  // set the attributes for link element
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css";
+  // Append link element to HTML head
+  head.appendChild(link);
+}
